6427a8cce6a25d9e66debb8e982a5f0a976558ea
Chrissy <christopher.reinbrecht@gmail.com>
2019-07-20 18:08:39 -0300

    Jenkins related fixes

--- vpc_game_jenkins.bat
+++ vpc_game_jenkins.bat
@@ -1,14 +1,6 @@
 @echo off
 
 devtools\bin\vpc.exe /ofd +game /2013
-
-for /D %%x in (*) do (
- cd %%x
- for %%y in (*.vcxproj) do (
-  echo %%y
-  "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" %%y
- )
- cd ..
-)
+type game_sln_append.txt >> openfortress.sln
 
 exit
