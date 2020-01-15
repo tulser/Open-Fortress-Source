0d3a14419f263b0e93d64b134b90cc732103b0ae
Nopey <golgothasTerror101@gmail.com>
2020-01-15 10:40:07 -0800

    Linux: LDPROXY list

--- ldproxy/README.txt
+++ ldproxy/README.txt
@@ -5,5 +5,5 @@ gameinfo.txt specifies a GameBin that is "open_fortress/bin/ldproxy/",
 which only contains our ldproxy shared object (client.so).
 When it is loaded, it checks the /proc/{}/map
 looking for itself, to find the path to open_fortress.
-With its own path in hand, it modifies LD_LIBRARY_PATH to include bin,
-loads the real client.so, and defers all CreateInterface calls to it.
+With its own path in hand, it loads all libraries listed in
+`open_fortress/bin/ldproxy.txt`.
--- ldproxy/ldproxy.c
+++ ldproxy/ldproxy.c
@@ -13,6 +13,9 @@ typedef void *(*CreateInterfaceFn)(const char *pName, int *pReturnCode);
 static void *g_client;
 static CreateInterfaceFn g_CreateInterface;
 
+static int third_party_count;
+static void **third_party_libs;
+
 static void load_lib(void)
 {
     printf("LDPROXY LOADING..\n");
@@ -32,6 +35,7 @@ static void load_lib(void)
             //sizeof("ldproxy/client.so\n")==18
             int clen = 18;
             if(end>=clen&&!strcmp(&path[end-clen], "ldproxy/client.so\n")){
+                printf("LDPROXY found self, line:\n%s", path);
                 //clip off the ldproxy/client.so\n
                 path[end-clen]='\0';
                 our_path = path;
@@ -40,26 +44,61 @@ static void load_lib(void)
                     our_path++;
                     our_path_len--;
                 }
+                printf("LDPROXY path to bin/:\n%s\n", our_path);
                 break;
             }
         }
     }
     assert(our_path);
-    // Step 2. Modify LD_LIBRARY_PATH
-    char *path = getenv("LD_LIBRARY_PATH");
-    char *newpath;
-    if(path){
-        int newlen = strlen(path) + 1 + our_path_len;
-        newpath = malloc(newlen);
-        snprintf(newpath, newlen, "%s:%s", our_path, path);
-    }else{
-        newpath = our_path;
+    // Step 2. Parse third_party_libs txt file
+    snprintf(line, sizeof(line), "%s/ldproxy.txt", our_path);
+    {
+        FILE *ldproxytxt;
+        assert(ldproxytxt = fopen(line, "r"));
+        third_party_count = 0;
+        while (fgets(line, sizeof(line), ldproxytxt))
+        {
+            //skip indentation
+            char *i;
+            for(i=line;*i==' ';i++);
+            //comments and empty lines
+            if(*i=='#' || *i == '\0') continue;
+            third_party_count++;
+        }
+        printf("LDPROXY third_party_count: %d\n", third_party_count);
+        third_party_libs = malloc(third_party_count*sizeof(void*));
+        rewind(ldproxytxt);
+        for (int x=0; fgets(line, sizeof(line), ldproxytxt);)
+        {
+            //skip indentation
+            char *i;
+            for(i=line;*i==' ';i++);
+            //comments and empty lines
+            if(*i=='#' || *i == '\0') continue;
+            int ilen = strlen(i);
+            if(i[ilen-1]=='\n') i[--ilen]='\0';
+            assert(ilen+our_path_len+1<sizeof(line));
+            memmove(line+our_path_len, i, ilen+1);
+            memcpy(line, our_path, our_path_len);
+            i = line+our_path_len;
+            printf("LDPROXY loading third_party_lib '%s', with path:\n%s\n", i, line);
+            third_party_libs[x] = dlopen(line, RTLD_NOW);
+            if(!third_party_libs[x]){
+                printf("LDPROXY FAILED! Couldn't open third_party_lib '%s'. DLERROR:\n%s\n", i, dlerror());
+                exit(1);
+            }
+            third_party_libs[x];
+            x++;
+        }
     }
-    setenv("LD_LIBRARY_PATH", newpath, true);
-    if(path) free(newpath);
     // Step 3. Load real client.so
     strcat(our_path, "client.so");
-    assert(g_client = dlopen(our_path, RTLD_NOW));
+    printf("LDPROXY loading %s\n", our_path);
+    g_client = dlopen(our_path, RTLD_NOW);
+    if(!g_client){
+        printf("LDPROXY FAILED! Couldn't open client.so. DLERROR:\n%s\n", dlerror());
+        exit(1);
+    }
     assert(g_CreateInterface = dlsym(g_client, "CreateInterface"));
     printf("LDPROXY SUCCESS\n");
 }
@@ -67,6 +106,10 @@ static void load_lib(void)
 static void unload_lib(void)
 {
     dlclose(g_client);
+    for(int x=0; x<third_party_count; x++){
+        dlclose(third_party_libs[x]);
+    }
+    free(third_party_libs);
 }
 
 __attribute__((visibility("default")))
