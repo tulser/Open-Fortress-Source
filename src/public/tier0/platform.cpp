2d272dd00c372aeaaf4d9a787e055bed11d768e4
Jan200101 <sentrycraft123@gmail.com>
2020-05-01 13:28:37 +0200

    add dedicated platform source code for cross platform functions

---
+++ src/public/tier0/platform.cpp
@@ -0,0 +1,34 @@
+#include "platform.h"
+
+#include <cstdio>
+#include <fstream>
+
+bool CopyFile(const char* source, const char* destination, bool dontoverwrite)
+{
+	if (dontoverwrite)
+	{
+		std::ifstream existcheck(destination);
+		if (existcheck.good()) return 0;
+
+	}
+	std::ifstream from(source, std::ios::binary);
+	std::ofstream to(destination, std::ios::binary);
+
+	to << from.rdbuf();
+
+	return 1;
+}
+
+bool DeleteFile(const char* file)
+{
+	return !std::remove(file);
+}
+
+void itoa(int val, char* buf, int base)
+{		
+	int i = 64;
+	
+	for(; val && i ; --i, val /= base)
+		buf[i] = "0123456789abcdef"[val % base];
+		val /= base;
+}
